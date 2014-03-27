b35c448eca9f27d39bc37498d6b747b5bb88a113
ffournier <florian.fournier@makina-corpus.com>
2014-03-27 14:39:05 +0100

    Add Comment and fix one error in file javascript

--- www/MBTilesPlugin.js
+++ www/MBTilesPlugin.js
@@ -1,33 +1,69 @@
+/*
+Declaration of the variable MBTilesPlugin to have acces to the plugin
+*/
 var MBTilesPlugin = function()
 {
 };
 
+/*
+function open
+open the given database sqlite
+params : {name : 'name of database', type : 'file' or 'db'}
+return : success or error callback
+*/
 MBTilesPlugin.prototype.open = function(params, onSuccess, onError)
 {
 	return cordova.exec(onSuccess, onError, "MBTilesPlugin", "open", [params]);
 };
 
+/*
+function getMetadata
+get the metadata of the database opened
+return : success or error callback
+*/
 MBTilesPlugin.prototype.getMetadata = function(onSuccess, onError)
 {
 	return cordova.exec(onSuccess, onError, "MBTilesPlugin", "get_metadata", []);
 };
 
+/*
+function getMinZoom
+get the min zoom of the database opened
+return : success or error callback
+*/
 MBTilesPlugin.prototype.getMinZoom = function(onSuccess, onError)
 {
 	return cordova.exec(onSuccess, onError, "MBTilesPlugin", "get_min_zoom", []);
 };
 
+/*
+function getMaxZoom
+get the max zoom of the database opened
+return : success or error callback
+*/
 MBTilesPlugin.prototype.getMaxZoom = function(onSuccess, onError)
 {
 	return cordova.exec(onSuccess, onError, "MBTilesPlugin", "get_max_zoom", []);
 };
 
+/*
+function getTile
+get tile in base 64 of the database opened with params
+params : {z:'z', x:'x', y:'y'} // coord
+return : success or error callback
+*/
 MBTilesPlugin.prototype.getTile = function(params, onSuccess, onError)
 {
 	return cordova.exec(onSuccess, onError, "MBTilesPlugin", "get_tile", [params]);
 };
 
-MBTilesPlugin.prototype.getTile = function(params, onSuccess, onError)
+/*
+function exectuteStatment
+execute query in the database opened
+params : {query:'query', params:{'param', 'param', 'param'}} 
+return : success or error callback
+*/
+MBTilesPlugin.prototype.exectuteStatment = function(params, onSuccess, onError)
 {
 	return cordova.exec(onSuccess, onError, "MBTilesPlugin", "execute_statment", [params]);
 };
